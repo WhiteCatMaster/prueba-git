0a496c1251ee815e9ff2cbf40449c3d79d20b80e
Iñigo Peleteiro <inigo.peleteiro@opendeusto.es>
2024-09-18 11:13:20 +0200

    me mueroooooooooooooooooooooooooooooooooo

--- pruebaGit/src/pruebaGit/ffff.java
+++ pruebaGit/src/pruebaGit/ffff.java
@@ -3,7 +3,7 @@ package pruebaGit;
 public class ffff {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		System.out.println("adiossssssssssssssssss mundo");
 
 	}
 
