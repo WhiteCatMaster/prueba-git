37a8d22492149cfc09f58e4c8aac0e1c4507a92f
Iñigo Peleteiro <inigo.peleteiro@opendeusto.es>
2024-09-23 08:18:17 +0200

    nuevo msg

--- pruebaGit/src/pruebaGit/ffff.java
+++ pruebaGit/src/pruebaGit/ffff.java
@@ -3,8 +3,20 @@ package pruebaGit;
 public class ffff {
 
 	public static void main(String[] args) {
-		System.out.println("se termina la clase");
+		System.out.println("tamoh a lunez");
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																														
